f733510b0db320e7feb50d6e9faf2c02e9a88f98
ladyGAV <gavrilina.av@edu.spbstu.ru>
2025-12-15 21:45:54 +0300

    Add files via upload

---
+++ lab_2.py
@@ -0,0 +1,22 @@
+import csv 
+import matplotlib.pyplot as plt 
+
+x,y = [], []
+with open('opendata.csv', 'r', encoding='utf-8') as file:
+    r = csv.reader(file, delimiter=',')
+
+    for row in r:
+        if len(row) == 4:
+            name = row[0]
+            region = row[1]
+            date = row[2]
+            value = row[3]
+        if '2018' in date and region == "Забайкальский край" and name == "Средняя пенсия":
+            x.append(date)
+            y.append(value)
+
+plt.plot(x,y)
+plt.xlabel("Дата")
+plt.ylabel("Средняя пенсия")
+plt.show()
+
